867cd9518aeece6592d70274e801f4368207630f
Liang Yi <liangyi2012@gmail.com>
2017-05-28 18:20:00 +0800

    follow 5.3

--- ch5/ch5-2.py
+++ ch5/ch5-2.py
@@ -2,32 +2,54 @@
 
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
-from ch5.util import *
 
 mnist = input_data.read_data_sets("../MNIST_data/", one_hot=True)
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 sess = tf.InteractiveSession(config=config)
 
 with tf.device('/gpu:0'):
+    # input
     x = tf.placeholder(tf.float32, [None, 784])
     y_ = tf.placeholder(tf.float32, [None, 10])
     x_image = tf.reshape(x, shape=[-1, 28, 28, 1])
     print('layer 0: {0}'.format(x_image.shape))
 
+    # conv1
     W_conv1 = weight_variable([5, 5, 1, 32])
     b_conv1 = bias_variable([32])
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
     print('conv 1: {0}'.format(h_pool1.shape))
 
+    # conv2
     W_conv2 = weight_variable([5, 5, 32, 64])
     b_conv2 = bias_variable([64])
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
     print('conv 2: {0}'.format(h_pool2.shape))
 
+    # fc1
     W_fc1 = weight_variable([7 * 7 * 64, 1024])
     b_fc1 = bias_variable([1024])
     h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
@@ -36,6 +58,7 @@ with tf.device('/gpu:0'):
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
     print('fc 1: {0}'.format(h_fc1.shape))
 
+    # fc2
     W_fc2 = weight_variable([1024, 10])
     b_fc2 = bias_variable([10])
     y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
